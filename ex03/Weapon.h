624e91b84c632c799968f5a9b77f2b83deaecec9
oussama7007 <oussamatachakourt@gmail.com>
2025-11-02 14:17:37 +0100

    Auto update: 2025-11-02 14:17:37

--- ex03/Weapon.h
+++ ex03/Weapon.h
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 16:45:50 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/01 16:52:04 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/02 13:50:12 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 class Weapon 
 {
     private:
-        std::string str;
+        std::string type;
     public:
         std::string &getType() const;
 }
