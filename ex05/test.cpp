38bf7b8d81c87d348b064d78c614d3cb4cad4c15
oussama7007 <oussamatachakourt@gmail.com>
2025-11-07 12:54:09 +0100

    still but good job

--- ex05/test.cpp
+++ ex05/test.cpp
@@ -6,25 +6,35 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 12:17:02 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/07 12:40:25 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/07 12:53:24 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-class Animal{
+class Light{
     public:
-        void greet() { std::cout << "HELLO , HUMAN\n";}
+        void on()  { std::cout << "💡 The light is ON\n"; }
+        void off() { std::cout << "🌑 The light is OFF\n"; }
+        void blink() { std::cout << "✨ The light is BLINKING\n"; }
 };
 int main()
 {
-    Robot r;
-    void    (Robot::*ptr)() = &Robot::greet;
-
-    (r.*ptr)();
-
-    Robot *f;
-    f = &r;
+    Light lamp;
+    std::string action[3] = {"on", "off", "blink"};
+    void   (Light::*funs[3])() = { &Light::on, &Light::off, &Light::blink };
     
-    (f->*ptr)();
+    std::string input;
+    std::cout << "Enter action (on/off/blink):" ;
+    std::cin >> input;
+
+    for(int i = 0 ; i < 3; i++)
+    {
+        if(input == action[i])
+        {
+            (lamp.*funs[i])();
+            return 0;
+        }
+    }
+    std::cout << "Unknown action!\n";
 }
