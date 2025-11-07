c8580a3066c088195dbefb5e0c916c325174d16e
oussama7007 <oussamatachakourt@gmail.com>
2025-11-07 14:40:41 +0100

    Auto update: 2025-11-07 14:40:41

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,16 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 10:26:54 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/07 10:26:55 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/07 14:29:35 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <Harl.h>
+
+
+void    Harl::debug()
+{
+        std::cout << "[DEBUG]\n";
+        std::cout << "I love having extra bacon for my 7XL-double-cheese
+            -triple-pickle-specialketchup burger. I really do!\n" << std::endl;
+}
