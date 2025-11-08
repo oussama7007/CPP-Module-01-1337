bbf29d8fc3be5f569f92bae773c539326b24e84b
oussama7007 <oussamatachakourt@gmail.com>
2025-11-08 15:12:59 +0100

    Auto update: 2025-11-08 15:12:59

--- ex00/Zombie.h
+++ ex00/Zombie.h
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 13:37:12 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/08 11:22:26 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/08 15:04:00 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ class Zombie
         ~Zombie(void); 
 };
 
-Zombie* newZombie( std::string name );
+Zombie newZombie( std::string name );
 void randomChump( std::string name );
 
 #endif
--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,14 +6,14 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 13:30:21 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/10/14 13:31:07 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/08 15:03:53 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "Zombie.h"
 
-Zombie* newZombie( std::string name )
+Zombie newZombie( std::string name )
 {
-    return new Zombie(name);
+    return  Zombie(name);
 }
